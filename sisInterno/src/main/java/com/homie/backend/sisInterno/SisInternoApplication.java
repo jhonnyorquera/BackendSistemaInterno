b6cbb0ca7a6550cac93e247cbc4b68101671ed1c
jhonnyorquera <49768684+jhonnyorquera@users.noreply.github.com>
2020-07-14 17:07:37 -0500

    Initial commit

---
+++ sisInterno/src/main/java/com/homie/backend/sisInterno/SisInternoApplication.java
@@ -0,0 +1,13 @@
+package com.homie.backend.sisInterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisInternoApplication.class, args);
+	}
+
+}
---
+++ sisInterno/src/test/java/com/homie/backend/sisInterno/SisInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homie.backend.sisInterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
