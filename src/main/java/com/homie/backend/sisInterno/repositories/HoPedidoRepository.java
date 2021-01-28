5f7343b5bef26a60e8ec48f5c262fa5b2606cd83
Orquera Ordonez Jhonny Orlando <jorquera@banecuador.fin.ec>
2021-01-28 18:19:05 -0500

    Update HoPedidoRepository.java

--- src/main/java/com/homie/backend/sisInterno/repositories/HoPedidoRepository.java
+++ src/main/java/com/homie/backend/sisInterno/repositories/HoPedidoRepository.java
@@ -22,26 +22,33 @@ public interface HoPedidoRepository extends CrudRepository<HoPedido, String> {
 	@Query("SELECT new com.homie.backend.sisInterno.dto.BusquedaResponseDto(p.peCodigo, cl.clNombre, p.peFechaPedido, p.peEstado, p.peCantidadHoras, p.peTipo, p.hoPedidoPadre.peCodigo ) from HoPedido p left OUTER JOIN p.hoCliente cl where p.peCodigo = ?1 ")
 	public List<BusquedaResponseDto> buscarPedidosXCodigo(String codigo);
 
+
 	@Query(value = " select pe.pe_codigo as PeCodigo, " + "cl.cl_nombre as ClCliente, "
 			+ "CAST (pe.pe_fecha_pedido AS date) as PeFecha, " + " pe.pe_estado as PeEstado , "
-			+ "pe.pe_cantidad_horas as PeCantidadHoras, " + "pe.pe_tipo as PeTipo, " + " pe.ho_pedido_padre as PePadre "
+			+ "pe.pe_cantidad_horas as PeCantidadHoras, " + "pe.pe_tipo as PeTipo, "
+			+ " pe.ho_pedido_padre as PePadre "
 			+ " from ho_pedido pe left outer join ho_cliente cl on pe.ho_cliente = cl.cl_id"
-			+ " where pe.pe_estado like :status and cl.cl_nombre like :cliente" + " and pe.pe_fecha_pedido > :inicio"
+			+ " where pe.pe_estado like :status and cl.cl_nombre like :cliente"
+			+ " and pe.pe_fecha_pedido > :inicio"
 			+ " and pe.pe_fecha_pedido < :fin", nativeQuery = true)
 	public List<BusquedaDtoIn> buscarPedidosXCamposInter(@Param("status") String status,
 			@Param("cliente") String cliente, @Param("inicio") Date fechaInicio, @Param("fin") Date fin);
-
-	@Query("select new com.homie.backend.sisInterno.dto.SaldosPagoDto"
-			+ "(p.peCodigo, p.peValor, "
-			+ "COALESCE((select sum(pa.ppValor) from pa where p.peCodigo=pa.hoPedido and pa.ppEstado = true"
-			+ "group by p.peCodigo),0), " + "cl.clNombre, p.peStatusPago) "
+	
+	@Query("select new com.homie.backend.sisInterno.dto.SaldosPagoDto(p.peCodigo, p.peValor, "
+			+ "COALESCE((select sum(pa.ppValor) from pa " + 
+			"									   where p.peCodigo=pa.hoPedido and pa.ppEstado = true" + 
+			"									   group by p.peCodigo),0), "
+			+ "cl.clNombre, p.peStatusPago) "
 			+ "from  HoCliente cl join HoPedido p on p.hoCliente = cl.clId left JOIN "
-			+ "HoPedidoPagos pa on p.peCodigo=pa.hoPedido where p.peTipo ='PRINCIPAL' "
-			+ "and p.peFechaPedido >=?1 and p.peFechaPedido<=?2 "
+			+ "HoPedidoPagos pa on p.peCodigo=pa.hoPedido where p.peTipo ='PRINCIPAL' and p.peFechaPedido >=?1 and p.peFechaPedido<=?2 "
 			+ "GROUP BY cl.clNombre, p.peCodigo ")
-	public List<SaldosPagoDto> saldosPago(Date fechaInicial, Date fechaFinal, String statusPago);
-
+	public List<SaldosPagoDto> saldosPago(Date fechaInicial, Date fechaFinal, String stado);
+	
 	@Query("select count(p.peCodigo) from HoPedido p where p.peFechaPedido BETWEEN ?1 AND ?2 and p.peEstado  not in ('CANCELADO')  ")
 	public int cantidadPedidos(Date inicio, Date fin);
+	
+	
+
+	
 
 }
